be9f6e34d98ea96569dd127f6071b04a0d095a8a
Cicheng Li <licicheng97@gmail.com>
2026-09-04 12:08:36 +0800

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/fwkt3/campusjobhub/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.fwkt3.campusjobhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
